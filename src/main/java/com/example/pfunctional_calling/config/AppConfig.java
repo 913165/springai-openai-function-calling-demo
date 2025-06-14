3221c8ff4ee8c9fd3bb21413653685bb64bb022f
913165 <tinumistry@gmail.com>
2025-06-15 01:22:32 +0530

    Update AppConfig.java

--- src/main/java/com/example/pfunctional_calling/config/AppConfig.java
+++ src/main/java/com/example/pfunctional_calling/config/AppConfig.java
@@ -1,21 +1,17 @@
-package com.example.pfunctional_calling.config;
+package com.example.springaiopenaifunctioncallingexample.config;
 
-
-import com.example.pfunctional_calling.service.MockBookingStatusService;
+import com.example.springaiopenaifunctioncallingexample.service.MockBookingStatusService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Description;
 
-import java.util.Map;
 import java.util.function.Function;
 
 @Configuration
 public class AppConfig {
 
     @Bean
-    @Description("Get the status of a hotel booking") // function description
-    public Function<MockBookingStatusService.BookingRequest, MockBookingStatusService.BookingResponse> bookingStatusFunction() {
+    public Function<MockBookingStatusService.BookingRequest,MockBookingStatusService.BookingResponse> bookingStatus(){
         return new MockBookingStatusService();
     }
-
 }
